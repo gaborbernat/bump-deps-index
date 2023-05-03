77551d49ba26fd859995ec275c9c14f3b1e69985
Bernát Gábor <bgabor8@bloomberg.net>
2023-05-03 16:05:09 -0700

    Fix HTML parser
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/bump_deps_index/_spec.py
+++ src/bump_deps_index/_spec.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 import json
+from collections import deque
 from enum import Enum, auto
 from functools import cache
-from io import StringIO
+from html.parser import HTMLParser
 from threading import Lock
 from urllib.request import urlopen
-from xml.etree.ElementTree import parse
 
 from packaging.requirements import Requirement
 from packaging.version import Version
@@ -89,36 +89,56 @@ def update_python(index_url: str, spec: str, pre_release: bool) -> str:
     return new_req
 
 
+class IndexParser(HTMLParser):
+    def __init__(self) -> None:
+        super().__init__()
+        self._at_tag: deque[str] = deque()
+        self._files: list[str] = []
+
+    @property
+    def files(self) -> frozenset[str]:
+        return frozenset(self._files)
+
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:  # noqa: U100
+        self._at_tag.append(tag)
+
+    def handle_endtag(self, tag: str) -> None:
+        if self._at_tag and self._at_tag[-1] == tag:  # pragma: no branch
+            self._at_tag.pop()
+
+    def handle_data(self, data: str) -> None:
+        if self._at_tag and self._at_tag[-1] == "a" and data.strip():
+            self._files.append(data.strip())
+
+
 def get_pkgs(index_url: str, package: str, pre_release: bool) -> list[Version]:
     with urlopen(f"{index_url}/{package}") as handler:
         text = handler.read().decode("utf-8")
 
-    root = parse(StringIO(text))
-
     versions: set[Version] = set()
-    for element in root.iter("a"):
-        if element.text:
-            file = element.text
-            if file.endswith(".tar.bz2"):
-                file = file[:-8]
-            if file.endswith(".tar.gz"):
-                file = file[:-7]
-            if file.endswith(".whl"):
-                file = file[:-4]
-            if file.endswith(".zip"):
-                file = file[:-4]
-            parts = file.split("-")
-            for part in parts[1:]:
-                if part.split(".")[0].isnumeric():
-                    break
-            else:
-                continue
-            try:
-                version = Version(part)
-            except ValueError:
-                pass
-            else:
-                versions.add(version)
+    parser = IndexParser()
+    parser.feed(text)
+    for file in parser.files:
+        if file.endswith(".tar.bz2"):
+            file = file[:-8]
+        if file.endswith(".tar.gz"):
+            file = file[:-7]
+        if file.endswith(".whl"):
+            file = file[:-4]
+        if file.endswith(".zip"):
+            file = file[:-4]
+        parts = file.split("-")
+        for part in parts[1:]:
+            if part.split(".")[0].isnumeric():
+                break
+        else:
+            continue
+        try:
+            version = Version(part)
+        except ValueError:
+            pass
+        else:
+            versions.add(version)
     return sorted((v for v in versions if (True if pre_release else not v.is_prerelease)), reverse=True)
 
 
